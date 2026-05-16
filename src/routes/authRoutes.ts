f8bd9892e86310af7e33f75d5c343e5b23fce8c6
Renss <rendyharvest@gmail.com>
2026-05-17 00:43:09 +0700

    feat: patch update user

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -20,7 +20,7 @@ router.post("/logout", logoutUser);
 // Private Routes (Cuma yang punya cookie token valid yang bisa akses)
 router.get("/users", protect, getAllUsers);
 router.get("/users/:id", protect, getUserById);
-router.put("/users/:id", protect, updateUser);
+router.patch("/users/:id", protect, updateUser);
 router.delete("/users/:id", protect, deleteUser);
 
 export default router;
