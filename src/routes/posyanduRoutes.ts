a925dc847f6afb8d0a306d8039e4e143dd6d39b7
Renss <rendyharvest@gmail.com>
2026-05-17 00:43:36 +0700

    feat: patch update posyandu

--- src/routes/posyanduRoutes.ts
+++ src/routes/posyanduRoutes.ts
@@ -17,13 +17,13 @@ const router = Router();
 // CRUD Balita
 router.post("/balita", protect, tambahBalita);
 router.get("/balita", protect, ambilSemuaBalita);
-router.put("/balita/:id", protect, updateBalita);
+router.patch("/balita/:id", protect, updateBalita);
 router.delete("/balita/:id", protect, hapusBalita);
 
 // CRUD Lansia
 router.post("/lansia", protect, tambahLansia);
 router.get("/lansia", protect, ambilSemuaLansia);
-router.put("/lansia/:id", protect, updateLansia);
+router.patch("/lansia/:id", protect, updateLansia);
 router.delete("/lansia/:id", protect, hapusLansia);
 
 export default router;
